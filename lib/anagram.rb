5d772bed4ae58110b58402bee53a7983617cc923
Tyler Hall <thall2980@gmail.com>
2022-10-11 19:35:00 -0400

    commit tree c85b0c22c53d8de3b01e3e0c0929f7de34555818

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(word)
+        @name = word
+    end
+
+    def match(array)
+    array.filter {|x| x.split("").sort == @name.split("").sort}
+    end
+end
